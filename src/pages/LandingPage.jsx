07268ad68b78c275934c3087e124cdaf4ab06a40
Vivek <vivek.k@ekoinfomatics.com>
2023-02-03 18:17:16 +0530

    completed 3 sections

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -8,9 +8,14 @@ const LandingPage = () => {
     },
 
     {
+      layout: "banner_text",
+      h1: "Data, like oil, is a valuable resource. However, unlike oil, data leaks can be just as detrimental.",
+    },
+
+    {
       layout: "left_image",
       image: "../survey_animation.gif",
-      h2: "Data, like oil, is a valuable resource. However, unlike oil, data leaks can be just as detrimental.",
+      h2: "With our survey tool, you have all the necessary features to produce high-quality surveys",
       p: {
         type: "bullet_points",
         content_array: [
@@ -30,28 +35,22 @@ const LandingPage = () => {
       },
     },
 
-    // {
-    //   layout: "right_image",
-    //   image: "../survey_animation.gif",
-    //   h2: "Data, like oil, is a valuable resource. However, unlike oil, data leaks can be just as detrimental.",
-    //   p: {
-    //     type: "bullet_points",
-    //     content_array: [
-    //       {
-    //         content:
-    //           "Customize your surveys to effectively communicate with your customers, employees, or target audience using our pre-made templates",
-    //       },
-    //       {
-    //         content:
-    //           "Gain access to a library of expertly crafted sample questions to enhance the quality of your surveys",
-    //       },
-    //       {
-    //         content:
-    //           "Our survey tool provides you with the best practices to create even the most complex surveys",
-    //       },
-    //     ],
-    //   },
-    // },
+    {
+      layout: "right_image",
+      image: "../right_image_1.svg",
+      h2: "Reach diverse range of individuals",
+      p: {
+        type: "paragraph",
+        content:
+          "Effortlessly connect with and gather feedback from your audience through multiple channels like email, website, social media, and more, with our user-friendly survey tool that features pre-written templates, enabling you to create and launch surveys, polls, or quizzes in minutes!",
+      },
+    },
+
+    {
+      layout: "get_started_banner",
+      h1: "Aren't you just a little curious?",
+      link: "/",
+    },
   ];
   return (
     <div className="font-nunito ">
@@ -79,7 +78,7 @@ const LandingPage = () => {
                       </span>
                     </h1>
 
-                    <p className="text-[#1E1E1E] py-20 sm:py-10 md:text-lg lg:text-xl md:w-[80%] mx-auto">
+                    <p className="text-[#1E1E1E] py-20 sm:py-10 md:py-20 md:text-lg lg:text-xl md:w-[80%] mx-auto">
                       Ensure the security of your respondents' information with
                       our survey tool that prioritizes data protection and
                       privacy.
@@ -98,17 +97,17 @@ const LandingPage = () => {
               )}
 
               {data?.layout === "left_image" && (
-                <section className="flex flex-col md:flex-row gap-5 md:items-center">
+                <section className="flex flex-col md:flex-row gap-5 md:items-start lg:items-center pb-20">
                   <div className="flex-1">
                     <img
-                      src="../survey_animation.gif"
+                      src={data?.image}
                       alt=""
-                      className="w-full"
+                      className="w-full  mt-[-25px] lg:mt-0"
                     />
                   </div>
 
                   <div className="flex-1">
-                    <h2 className="text-xl md:text-lg font-semibold mb-10">
+                    <h2 className="text-xl md:text-lg lg:text-xl xl:text-2xl font-bold mb-10">
                       {data?.h2}
                     </h2>
                     <div>
@@ -119,9 +118,9 @@ const LandingPage = () => {
                               return (
                                 <div
                                   key={content_index}
-                                  className="flex gap-2 items-start my-5"
+                                  className="flex gap-2 items-start my-5 text-gray-800"
                                 >
-                                  <div className="w-[6px] mt-2 rounded-full aspect-square bg-[#1e2124]"></div>
+                                  <div className="w-[6px] mt-2 rounded-full aspect-square bg-gray-800"></div>
                                   <p className="w-[calc(100%-5px)] ">
                                     {content_data?.content}
                                   </p>
@@ -131,8 +130,80 @@ const LandingPage = () => {
                           )}
                         </div>
                       )}
+
+                      {data?.p?.type === "paragraph" && (
+                        <div className="my-5 ">
+                          <p className="w-[calc(100%-5px)] ">{data?.content}</p>
+                        </div>
+                      )}
                     </div>
                   </div>
+                </section>
+              )}
+
+              {data?.layout === "right_image" && (
+                <section className="flex flex-col md:flex-row-reverse gap-5 md:items-start lg:items-center pb-20">
+                  <div className="flex-1">
+                    <img
+                      src={data?.image}
+                      alt=""
+                      className="w-full max-w-[400px]  mx-auto  mt-[-25px] lg:mt-0"
+                    />
+                  </div>
+
+                  <div className="flex-1">
+                    <h2 className="text-xl md:text-lg lg:text-xl xl:text-2xl font-bold mb-10">
+                      {data?.h2}
+                    </h2>
+                    <div>
+                      {data?.p?.type === "bullet_points" && (
+                        <div className="my-5 ">
+                          {data?.p?.content_array?.map(
+                            (content_data, content_index) => {
+                              return (
+                                <div
+                                  key={content_index}
+                                  className="flex gap-2 items-start my-5 text-gray-800"
+                                >
+                                  <div className="w-[6px] mt-2 rounded-full aspect-square bg-gray-800"></div>
+                                  <p className="w-[calc(100%-5px)] ">
+                                    {content_data?.content}
+                                  </p>
+                                </div>
+                              );
+                            }
+                          )}
+                        </div>
+                      )}
+
+                      {data?.p?.type === "paragraph" && (
+                        <div className="my-5 ">
+                          <p className="w-[calc(100%-5px)] ">
+                            {data?.p?.content}
+                          </p>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </section>
+              )}
+
+              {data?.layout === "banner_text" && (
+                <section>
+                  <h1 className="text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold text-center w-[90%] xl:w-[80%] 2xl:w-[70%]    text-gray-900 mx-auto">
+                    {data?.h1}
+                  </h1>
+                </section>
+              )}
+
+              {data?.layout === "get_started_banner" && (
+                <section>
+                  <h1 className="text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold text-center w-[90%] xl:w-[80%] 2xl:w-[70%]    text-gray-900 mx-auto">
+                    {data?.h1}
+                  </h1>
+                  <button className="py-3 sm:py-2 rounded-xl px-5 min-w-[100px]  text-white font-medium shadow-[#038bfe] shadow-md  bg-[#038BFE] active:scale-95 transition-all mx-auto block mt-10 mb-20">
+                    Get Started
+                  </button>
                 </section>
               )}
             </div>
